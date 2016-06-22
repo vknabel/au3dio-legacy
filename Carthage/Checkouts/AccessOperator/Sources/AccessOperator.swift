ed284588267d360b77676ecf6fe8d2a64fc4b20a
Valentin Knabel <develop@vknabel.com>
2016-06-22 14:22:56 +0200

    Updated dependencies

--- Carthage/Checkouts/AccessOperator/Sources/AccessOperator.swift
+++ Carthage/Checkouts/AccessOperator/Sources/AccessOperator.swift
@@ -1,44 +1,45 @@
 
-protocol AccessOperator {
+public protocol AccessOperator {
     associatedtype Target
     associatedtype Result
 
     func update(inout target: Target?, withAccess: (inout result: Result?) -> Void) -> Void
 }
 
-extension AccessOperator {
-    func chaining<T: AccessOperator where T.Target == Self.Result>
-        (access: T) -> ComposedAccessOperator<Self, T> {
-        return ComposedAccessOperator(argument: (self, access))
+public extension AccessOperator {
+    public func chaining<T: AccessOperator where T.Target == Self.Result>
+        (access: T) -> AnyAccessOperator<Target, T.Result> {
+        let composed = ComposedAccessOperator(argument: (self, access))
+        return AnyAccessOperator<Target, T.Result>(of: composed)
     }
 }
 
-struct AnyAccessOperator<T, R>: AccessOperator {
-    typealias Target = T
-    typealias Result = R
+public struct AnyAccessOperator<T, R>: AccessOperator {
+    public typealias Target = T
+    public typealias Result = R
 
-    var updater: ((inout T?, (inout R?) -> Void) -> Void)
-    init<O: AccessOperator where O.Target == T, O.Result == R>(of accessOperator: O) {
+    public let updater: ((inout T?, (inout R?) -> Void) -> Void)
+    public init<O: AccessOperator where O.Target == T, O.Result == R>(of accessOperator: O) {
         self.updater = accessOperator.update
     }
 
-    func update(inout target: T?, withAccess: (result: inout R?) -> Void) {
+    public func update(inout target: T?, withAccess: (result: inout R?) -> Void) {
         self.updater(&target, withAccess)
     }
 }
 
-struct ComposedAccessOperator<T: AccessOperator, R: AccessOperator where T.Result == R.Target>: AccessOperator {
-    typealias Target = T.Target
-    typealias Argument = (T, R)
-    typealias Result = R.Result
+public struct ComposedAccessOperator<T: AccessOperator, R: AccessOperator where T.Result == R.Target>: AccessOperator {
+    public typealias Target = T.Target
+    public typealias Argument = (T, R)
+    public typealias Result = R.Result
 
-    let inputOperator: T
-    let outputOperator: R
-    init(argument: (T, R)) {
+    public let inputOperator: T
+    public let outputOperator: R
+    public init(argument: (T, R)) {
         (inputOperator, outputOperator) = argument
     }
 
-    func update(inout target: Target?, withAccess: (inout result: Result?) -> Void) -> Void {
+    public func update(inout target: Target?, withAccess: (inout result: Result?) -> Void) -> Void {
         inputOperator.update(&target) { (result) in
             self.outputOperator.update(&result, withAccess: withAccess)
         }
